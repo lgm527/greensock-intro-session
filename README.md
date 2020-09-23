61572273da164863dc348b8c58ffbfd860f9f3bc
lgm527 <laurellmccaffrey@gmail.com>
2020-09-23 15:02:00 -0400

    notes added

---
+++ README.md
@@ -0,0 +1,5 @@
+# GreenSock Animations, JavaScriptMarathon.com (This Dot Media)
+
+https://www.youtube.com/watch?v=ovRXOZogpzw&feature=youtu.be
+
+https://dev.to/coffeecraftcode/challenge-get-started-with-gsap-3-4ie3
---
+++ script.js
@@ -0,0 +1,38 @@
+const title = 'GreenSock Animations, JavaScriptMarathon.com (This Dot Media)'
+
+// Christina Gorton (Open Source community manager at dev.to/Forum) @coffeecraftcode
+
+// -----------------
+
+// Some Terms...
+// GreenSock/GSAP (js and company)
+// SVGs (scalable vector graphic, always crisp)
+// Tweens (single movement in animation, "inbetween state") 
+gsap.method(element, varsObject);
+gsap.to()
+gsap.from()
+gsap.fromTo()
+var element = 'want you want to animate, can be single thing or array of DOM elements, can be selected with CSS selector, variable, ref, etc'
+var varsObject = 'properties you want to animate in key/value pairs'
+
+// Timelines: combine multiple Tweens
+gsap.timeline();
+// Performance is similar to CSS and whatever animations cause repaints are more costly
+// Figma export SVG, ✓ contents only, ✓ include "id" attributes = groupings
+// svg in html file
+// animations in js file
+
+let article = 'https://dev.to/coffeecraftcode/challenge-get-started-with-gsap-3-4ie3'
+
+let youtubeSession = 'https://www.youtube.com/watch?v=ovRXOZogpzw&feature=youtu.be'
+
+let svgCollection = 'https://undraw.co/'
+
+let scrollTriggerExample = 'https://codepen.io/cgorton/pen/oNjrMJb'
+
+let codePens = {
+    'AnimatinDivsGreenSock': 'https://codepen.io/lgm527/pen/mdPvbWJ',
+    'SVGShapes': 'https://codepen.io/lgm527/pen/xxVMKQo',
+    'Devices': 'https://codepen.io/lgm527/pen/dyMabrY',
+};
+
